256bf501473ab0ceb7aec707d400bacfd720d491
dino1729 <dino.feynman@gmail.com>
2023-10-13 19:27:35 -0700

    working code

--- test_scripts/picamera.py
+++ test_scripts/picamera.py
@@ -1,7 +1,7 @@
 import cv2
 
-# Load the pre-trained palm cascade classifier
-palm_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'palm.xml')
+# Load the pre-trained fist cascade classifier
+fist_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fist.xml')
 
 # Initialize the camera
 camera = cv2.VideoCapture(0)  # Change the index to 1 if you're using a USB camera
@@ -13,21 +13,18 @@ while True:
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect palms in the frame
-    palms = palm_cascade.detectMultiScale(gray, 1.1, 5)
+    # Detect fists in the frame
+    fists = fist_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    # Draw rectangles around the detected palms
-    for (x, y, w, h) in palms:
+    # Draw rectangles around the detected fists
+    for (x, y, w, h) in fists:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, 'Detected Palm', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    # Display the frame with detected palms
-    cv2.imshow('Palm Detection', frame)
+        cv2.putText(frame, 'Detected fist', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        print("fist detected!")
 
     # Check for the 'q' key to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close all windows
+# Release the camera
 camera.release()
-cv2.destroyAllWindows()
